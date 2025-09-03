469c48fea08c887c3b0f74da7bf328f59ea71be5
crazydjon2 <roflea5@gmail.com>
2025-09-03 17:20:08 +0600

    DB env fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,21 +11,25 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { CardsModule } from './cards/cards.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesTypesModule } from './categories_types/categories-types.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Сделать переменные окружения доступными глобально
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'user',
-      password: 'password',
-      database: 'mydatabase',
+      host: process.env.DATABASE_HOST || 'localhost', // Используем переменную окружения
+      port: process.env.DATABASE_PORT ? +process.env.DATABASE_PORT : 5432, // Парсим в число
+      username: process.env.DATABASE_USER || 'user',
+      password: process.env.DATABASE_PASSWORD || 'password',
+      database: process.env.DATABASE_NAME || 'mydatabase',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
       namingStrategy: new SnakeNamingStrategy(),
     }),
+    UserModule,
     SeederModule,
     WordsModule,
     CategoriesModule,
