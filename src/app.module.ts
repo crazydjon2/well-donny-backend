c2e0e1fc6cd82fcd0a5fd41c2282a0e636d7e8e9
Kirill <roflea5@gmail.com>
2025-11-30 22:31:25 +0600

    i18n fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,7 +37,7 @@ import { ErrorService } from './common/services/error.service';
     I18nModule.forRoot({
       fallbackLanguage: 'en',
       loaderOptions: {
-        path: path.resolve(process.cwd(), 'src', 'i18n'),
+        path: path.join(__dirname, 'i18n'),
         watch: true,
       },
       resolvers: [QueryResolver, new HeaderResolver(['x-lang'])], // Определяет язык по query-параметру, e.g. ?lang=ru
